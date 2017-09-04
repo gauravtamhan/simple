4e80a200be5fcc06593e082781cdfbb625d6e9c5
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2017-09-04 15:30:54 -0400

    Forgot to link new content location in these files

--- category.php
+++ category.php
@@ -21,7 +21,7 @@
       <?php
       if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-        get_template_part( 'content', get_post_format() );
+        get_template_part( 'template-parts/posts/content', get_post_format() );
 
       endwhile; endif;
       ?>
